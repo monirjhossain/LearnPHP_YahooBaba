a60c803dce9f4c46042107c60909ca49017d2fee
Md. Monir Hossain <59936758+monirjhossain@users.noreply.github.com>
2021-11-09 23:26:14 +0600

    09.12.2021

---
+++ Class 6/DataTypes.php
@@ -0,0 +1,16 @@
+<?php 
+
+	$x = "Hello World"; --------------- String
+
+	$x = 25; -------------------------- Integer 
+
+	$x = 20.23; ----------------------- float 
+
+	$x = true; ------------------------ boolean 
+
+	$x = Array("HTML","CSS","JS"); ---- Array 
+
+	$x = new MyClass(); --------------- Object 
+
+	$x = null ------------------------- Null 
+ ?>
---
+++ Class 8/constant.php
@@ -0,0 +1,9 @@
+<?php 
+
+	define("Shohrab", 50, true);
+
+	echo shohrab;
+
+?>
+<!-- Jokhon case sensitive er dorkar hoy tokhon amra true parameter er maddhome case sensitive er problem
+thik korte pari... ar Constant er value kokhono change kora jay na.. -->
